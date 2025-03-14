ed8c6a3a3344372e89d226e8ae731e671ad28ae1
Elanrif <saidbacoelanrif@gmail.com>
2025-03-14 18:19:54 +0000

    feat: landing page elanrif

--- component/landing-page.tsx
+++ component/landing-page.tsx
@@ -3,7 +3,26 @@ import React from 'react'
 export default function Page() {
   return (
     <div>
-        je suis sur le composant landing page
+      <div className='flex justify-between'>
+        <div className="flex gap-4">
+          <p className="font-medium text-lg"> Community</p>
+          <p className="font-medium text-lg"> Community</p>
+          <p className="font-medium text-lg"> Community</p>
+          <p className="font-medium text-lg"> Community</p>
+        </div>
+
+        <div className="flex gap-3">
+          <button className="px-4 py-2 border w-16 bg-slate-300 text-slate-600 border-black rounded-xl">
+            {" "}
+            Search
+          </button>
+          <p> Log in</p>
+          <button className="px-4 py-2 border border-black rounded-xl">
+            {" "}
+            sign up
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
